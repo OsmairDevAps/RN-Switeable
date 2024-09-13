11ba9beed305ba24e922b1ee5d493ac53ed8b63a
OsmairDevAps <osmairdev@gmail.com>
2024-09-13 10:03:18 -0300

    Initial Project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { Home } from "./src/app/home";
+import { Header } from "./src/components/header";
 
 export default function App() {
   return (
     <GestureHandlerRootView>
+      <Header />
       <Home />
     </GestureHandlerRootView>
   );
---
+++ src/components/header/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    height: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#ffffff',
+  }
+})
